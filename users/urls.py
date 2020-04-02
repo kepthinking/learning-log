6f175d3974691bb2eac162e86da66b67d6539ee7
ehmatthes <eric@example.com>
2020-04-02 15:42:27 +0800

    Ready for deployment to heroku.

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+
+"""微应用程序users定义urls模式"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # 登录页面
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'),name='login'),
+    # 注销
+    url(r'^logout/$', views.logout_view, name='logout'),
+    # 注册页面
+    url(r'^register/$', views.register, name='register'),
+]
